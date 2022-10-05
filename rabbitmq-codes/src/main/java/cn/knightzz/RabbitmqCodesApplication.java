3a4594db7ea1ed3f91540393ab266dd0274563c0
王天赐 <15565946702@163.com>
2022-10-05 20:43:31 +0800

    RabbitMQ学习

---
+++ rabbitmq-codes/src/main/java/cn/knightzz/RabbitmqCodesApplication.java
@@ -0,0 +1,13 @@
+package cn.knightzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqCodesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqCodesApplication.class, args);
+    }
+
+}
